081914c211583d06d3b5c54c0b0d4b97223615f6
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-15 13:16:38 +0200

    componente header con navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <!-- NAVBAR  -->
+    <nav class="navbar">
+      <div class="container py-4">
+        <img
+          src="@/assets/img/avadabarbers-logo-200x70.png"
+          alt="logo"
+          class=""
+        />
+        <div>
+          <a class="navbar-brand" href="#">
+            <i class="fas fa-shopping-cart fs-4"></i>
+          </a>
+          <a class="navbar-brand" href="#">
+            <i class="fas fa-bars fs-4"></i>
+          </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
